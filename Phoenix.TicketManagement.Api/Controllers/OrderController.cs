cd6cdb3f881e1231a9d848933d31d824f68fa29c
Arpit Patel <Arpit.ce27@outlook.com>
2023-04-09 17:29:29 -0400

    Added Order API

--- Phoenix.TicketManagement.Api/Controllers/OrderController.cs
+++ Phoenix.TicketManagement.Api/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Phoenix.TicketManagement.Application.Features.Orders.Queries.GetOrdersForMonth;
 
 namespace Phoenix.TicketManagement.Api.Controllers
 {
@@ -11,6 +12,17 @@ namespace Phoenix.TicketManagement.Api.Controllers
         public OrderController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpGet("/getpagedordersformonth", Name = "GetPagedOrdersForMonth")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult<PagedOrdersForMonthVm>> GetPagedOrdersForMonth(DateTime date, int page, int size)
+        {
+            var getOrdersForMonthQuery = new GetOrdersForMonthQuery() { Date = date, Page = page, Size = size };
+            var dtos = await _mediator.Send(getOrdersForMonthQuery);
+
+            return Ok(dtos);
         }
     }
 }
